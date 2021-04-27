5314e5c3c6966fdb92a6ea6394c32692a181b978
Hierro Nabit <roninabit@gmail.com>
2021-04-28 03:01:58 +0700

    add cors

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEM_LOADING } from './types';
 
+const url = "https://servershopping.herokuapp.com/";
 export const  getItems = () => dispatch => {
     dispatch(setItemLoading());
-    axios.get('/api/items').then(res => dispatch({
+    axios.get(url +'api/items').then(res => dispatch({
             type: GET_ITEMS,
             payload: res.data
     }));
 }
 export const  addItem = (item) => dispatch => {
-   axios.post('api/items', item).then(res => dispatch({
+   axios.post(url +'api/items', item).then(res => dispatch({
             type: ADD_ITEM,
             payload: res.data
    }));
 }
 export const  deleteItem = (id) => dispatch => {
-    axios.delete(`/api/items/${id}`).then(res => dispatch({
+    axios.delete(url +`api/items/${id}`).then(res => dispatch({
         type: DELETE_ITEM,
         payload: id
     }));
